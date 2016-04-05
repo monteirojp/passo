5f47b74f9b1a50d5d37bc5f26b143aa023ef0070
Rodrigo Monteiro <monteirojp@gmail.com>
2016-04-05 21:55:15 +0200

    Added files via upload
    Paramentizando (passo a 0,0888º)

--- Passo.c
+++ Passo.c
@@ -5,26 +5,43 @@
 #include <flex_lcd.c>
 #define frame_time      0                  //Velocidade do motor
 
-int8 i = 3, i_1 = 0,j;
-int16 ad_antigo, ad_atual, total_passos, posicoes_na_escala;
-signed int16 pos_atual =0;
-int16 passos_dados =0;
-int16 ultimaRPM = 0;
-int16 rpm=0;
-float passos_por_posicao;
+int16 i = 3, i_1 = 0,j;
+int16 ad_antigo, ad_atual;
+signed int16 pos_atual =-1;
 int8 y;
 signed int16 distancia;
 
-char motor[4]=
+float    angulo_passo = 0.088;
+float    angulo_abertura = 300;
+int16    posicoes_na_escala = 60; //Simulando um contagiros até 6000 RPM
+int16    passos_dados =0;
+int16    ultimaRPM = 0;
+int16    rpm=0;
+float    passos_por_posicao, total_passos;
+
+
+
+char motor[8]=
   {
+  
      0b1100, //PASSO1
      0b0110, //PASSO2
      0b0011, //PASSO3
-     0b1001, //PASSO4      
+     0b1001, //PASSO4  
+         
   };  
  
-//int16 rpms[5]={9,35,9,20,65};
-   
+ /* Meio Passo   
+     0b1000,
+     0b1100,
+     0b0100,
+     0b0110,
+     0b0010,
+     0b0011,
+     0b0001,
+     0b1001, */
+ 
+ /*    */  
   
   
 void avanca(int16 qtdPassos){
@@ -39,7 +56,8 @@ void avanca(int16 qtdPassos){
          pos_atual = 0;
       } 
       output_b(motor[pos_atual]);            
-      delay_ms (3);
+      
+      delay_ms (5);
    }   
 }
 
@@ -59,27 +77,25 @@ void retrocede(int16 qtdPassos){
          pos_atual = 3;
       } 
       output_b(motor[pos_atual]);              
-      delay_ms (3);
+      delay_ms (5);
    }        
 }
 
 void setaRPM(int16 rpm){        
-   if(rpm>ultimaRPM){   
-      avanca(rpm*passos_por_posicao);
+   int16 rpmPassos=rpm*passos_por_posicao;
+   if(rpm>ultimaRPM){      
+      avanca(rpmPassos);
       ultimaRPM=rpm;
    }else if(rpm<ultimaRPM){
-      retrocede(rpm*passos_por_posicao);
+      retrocede(rpmPassos);
       ultimaRPM=rpm;
-   }
-   printf(lcd_putc,"\f%lu RPM",ultimaRPM*100); 
-   
+   }      
 }
 
 void main ()
 { 
-   posicoes_na_escala = 60; //Simulando um contagiros até 6000 RPM
-   total_passos = 167; //Total de posições do motor entre o marcador 0 até 6 (passo a 1,8º)
-   passos_por_posicao = (float)total_passos/posicoes_na_escala; //Total de passos para cada posição no conta giros (a agulha anda de 50 em 50 RPM)
+   total_passos = angulo_abertura/angulo_passo; //Total de posições do motor entre o marcador 0 até 6 (passo a 0,088º)
+   passos_por_posicao = total_passos/posicoes_na_escala; //Total de passos para cada posição no conta giros (a agulha anda de 50 em 50 RPM)
    lcd_init();
    delay_ms (100);
   
@@ -87,35 +103,24 @@ void main ()
    SETUP_ADC(ADC_CLOCK_DIV_16);
   
    set_adc_channel(0);
-   delay_us (20);
+   delay_us (20);        
    
    printf(lcd_putc,"\fPosicionando...");   
-   avanca(117); // 0 RPM   
+   avanca(2387); // 0 RPM   
    passos_dados=0;   
    printf(lcd_putc,"\f0 RPM");
-   
-        
- /*  for (i_1=0;i_1<5;i_1++){     
-      delay_us (300);
-      setaRPM(rpms[i_1]);
-   }*/
-
   
   while(true){
-
       ad_atual = read_adc();
       
       rpm = ad_atual/15;
       
-      setaRPM(rpm);
-     
       printf(lcd_putc,"\f%lu RPM  ad: %lu",rpm*100, ad_atual );
-     
-     
-     delay_ms (20);
-     
-  }
-  
+      
+      setaRPM(rpm);                 
+      
+      delay_ms (20);
+  }  
 }
 
   /*
